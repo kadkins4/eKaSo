9cab0ccddc4f26522096da4b385b526383a64859
Kendall <adkins.kendall90@gmail.com>
2017-09-11 15:29:41 -0400

    pre react

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -7,4 +7,20 @@ class TripsController < ApplicationController
   def show
     @trip = Trip.find(params[:id])
   end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.create!(trip_params)
+
+    redirect_to trip_path
+    flash[:notice] = "#{@trip} created, have fun!"
+  end
+
+private
+  def trip_params
+    params.require(:trip).permit(:name, :location, :description, :photo_url, :date)
+  end
 end
